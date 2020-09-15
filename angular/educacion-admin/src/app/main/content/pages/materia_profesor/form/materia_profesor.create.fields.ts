5e51afd24348fb36eb5c1bbc19305c834b7989ae
spirit-desarrollo <66275244+spirit-desarrollo@users.noreply.github.com>
2020-09-15 15:27:19 -0300

    Update materia_profesor.create.fields.ts

--- angular/educacion-admin/src/app/main/content/pages/materia_profesor/form/materia_profesor.create.fields.ts
+++ angular/educacion-admin/src/app/main/content/pages/materia_profesor/form/materia_profesor.create.fields.ts
@@ -37,28 +37,55 @@ export const MATERIA_PROFESOR_CREATE_FORM_FIELDS_DEF = [
     controlType: 'hidden'
   },
 */
-  {
-    key: 'materia',
-    labelKey: 'materia_profesor_create_form_fields_def_field_materia',
-    controlType: 'autocomplete',
-    label: 'Materia',
-    required: true,
-    options: {
-      transferIdToField: 'idMateria',
-      elementLabel: 'descripcionNivel',
+{
+  key: 'idNivel',
+  labelKey: 'materia_profesor_create_form_fields_def_field_nivel',
+  required: true,
+  controlType: 'select',
+  options: {
+      elementLabel: 'descripcion',
       elementValue: 'id',
-      useNativeFilter: false,
-      selectElementOrCleanField: 'Debe seleccionar un elemento o limpiar el campo'
-    },
-    apiOptions: {
-      queryString: {
-        descripcion: 'materia'
-      },
-      url: PREFIX_DOMAIN_API_EDUCACION + '/materias/'
-    }
+      fromWs: {
+        key: 'materia_profesor_create_form_fields_def_field_nivel',
+        url: PREFIX_DOMAIN_API_EDUCACION + '/niveles/'
+      }
   },
-  { 
-    key: 'idMateria',   
-    controlType: 'hidden'
-  }
+  dependencyKey: 'idMateria'
+},
+{
+  key: 'idMateria',
+  labelKey: 'materia_profesor_create_form_fields_def_field_materia',
+  required: true,
+  controlType: 'select',
+  options: {
+      elementLabel: 'descripcion',
+      elementValue: 'id',
+      fromWs: {
+        key: 'materia_profesor_create_form_fields_def_field_materia',
+        url: PREFIX_DOMAIN_API_EDUCACION + '/materias/nivel/'
+      }
+  },
+  stopLoad: true
+},
+  // {
+  //   key: 'materia',
+  //   labelKey: 'materia_profesor_create_form_fields_def_field_materia',
+  //   controlType: 'autocomplete',
+  //   label: 'Materia',
+  //   required: true,
+  //   options: {
+  //     transferIdToField: 'idMateria',
+  //     elementLabel: 'descripcionNivel',
+  //     elementValue: 'id',
+  //     useNativeFilter: false,
+  //     selectElementOrCleanField: 'Debe seleccionar un elemento o limpiar el campo'
+  //   },
+  //   apiOptions: {
+  //     queryString: {
+  //       descripcion: 'materia'
+  //     },
+  //     url: PREFIX_DOMAIN_API_EDUCACION + '/materias/'
+  //   },
+  //   dependencyId: 'idMateria'
+  // },
 ];
