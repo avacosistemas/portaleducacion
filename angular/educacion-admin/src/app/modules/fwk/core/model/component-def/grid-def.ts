031914fca5e77588fd944053017146d4655e7f1b
spirit-desarrollo <66275244+spirit-desarrollo@users.noreply.github.com>
2020-09-15 20:14:03 -0300

    Update grid-def.ts

--- angular/educacion-admin/src/app/modules/fwk/core/model/component-def/grid-def.ts
+++ angular/educacion-admin/src/app/modules/fwk/core/model/component-def/grid-def.ts
@@ -19,6 +19,8 @@ export class GridDef{
     fromArrayField?: string;
     // Deprecated use delleteAction
     deleteColumn?: any;    
+    deleteTernaria?: boolean;   
+    columnsTernaria?: string[];
     titleKey?: string;
     title?: string;
 }
